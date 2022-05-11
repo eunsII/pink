7ffd00b9aa6fd72cdbcde931d132ae8003a3bb04
y <y@DESKTOP-1E4JF9F>
2022-05-11 11:31:28 +0900

    avatar ...

--- whistle/src/main/java/com/githrd/test/sql/MemberSQL.java
+++ whistle/src/main/java/com/githrd/test/sql/MemberSQL.java
@@ -1,7 +1,9 @@
 package com.githrd.test.sql;
 
 public class MemberSQL {
-	public final int SEL_LOGIN_CNT = 1001;
+	public final int SEL_LOGIN_CNT 		= 1001;
+	public final int SEL_MEMBER_INFO 	= 1002;
+	public final int SEL_AVT_INFO 		= 1003;
 	
 	public String getSQL(int code) {
 		StringBuffer buff = new StringBuffer();
@@ -15,6 +17,25 @@ public class MemberSQL {
 			buff.append("	isshow = 'Y' ");
 			buff.append("	AND id = ? ");
 			buff.append("	AND pw = ? ");
+			break;
+		case SEL_AVT_INFO:
+			buff.append("SELECT ");
+			buff.append("	ano, savename, dir, gen ");
+			buff.append("FROM ");
+			buff.append("	avatar ");
+			buff.append("WHERE ");
+			buff.append("	isshow = 'Y' ");
+			buff.append("	AND ano = ? ");
+			break;
+		case SEL_MEMBER_INFO:
+			buff.append("SELECT ");
+			buff.append("	mno, name, id, mail, tel, m.gen, joindate, ano, savename ");
+			buff.append("FROM ");
+			buff.append("	member m, avatar ");
+			buff.append("WHERE ");
+			buff.append("	m.isshow = 'Y' ");
+			buff.append("	AND avt = ano ");
+			buff.append("	AND id = ? ");
 			break;
 		}
 		return buff.toString();
